f7e1c0e9e6050332d2b38cfbea23224121212c05
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-12-13 15:29:46 +0900

    chore: 배포환경설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+    tsconfigPaths(),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.tsx'),
+      name: '(라이브러리 이름)',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'styled-components', '**/*.stories.tsx'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'styled-components': 'styled',
+        },
+        banner: '"use client";',
+        interop: 'compat',
+      },
+    },
+    ssr: false,
+  },
+});
